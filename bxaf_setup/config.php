d28370952ef9eb54267b38cc02d8e1fb0f68766b
Baohong Zhang <bzhang1@ngs.biogen.com>
2021-05-01 14:02:24 -0400

    Add .gitignore

--- bxaf_setup/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once('default_settings_all.php');
-include_once('default_settings_omicsview.php');
-include_once('config_human_4.0_omicsview.php');
-include_once('override.php');
-
-?>
